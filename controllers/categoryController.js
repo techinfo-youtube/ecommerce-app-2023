9ef5fd57196bf249e19b4b9075a0c52ba85e0898
Hajra Shahid <hajra.mshahid24@gmail.com>
2023-05-04 23:18:37 +0300

    Initial

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,23 @@
-export const createCategoryController = () => {
-    
+import categoryModel from "../models/categoryModel";
+
+export const createCategoryController = async (req, res) => {
+    try {
+        const [name] = req.body
+        if(!name) {
+            return res.status(401).send({message: 'Name is required'});
+        }
+        const existingCategory = await categoryModel.findOne({name})
+
+        if(existingCategory) {
+            return 
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            error,
+            message: 'Error in Category'
+        })
+    }
 }
